7d02af6aeccb26dc181621a98a266b067e744d6e
ariel <arielz742@gmail.com>
2022-05-10 13:00:50 +0300

    for u

--- stack.h
+++ stack.h
@@ -3,12 +3,15 @@
 #include <string.h>
 #include "memory.h"
 
+pthread_mutex_t lock;
+
 typedef struct Node{
     char value[1024];
     struct Node* next;
 }Node;
 
 void PUSH(Node** root, char value[1024]){
+    pthread_mutex_lock(&lock);
     Node* new_node = malloc(sizeof(Node));
     int i = 0;
     if(new_node ==  NULL){
@@ -24,17 +27,21 @@ void PUSH(Node** root, char value[1024]){
         curr = curr->next;
     }
     curr->next = new_node;
+    pthread_mutex_unlock(&lock);
 }
 
 char* TOP(Node** root){
+    pthread_mutex_lock(&lock);
     Node* curr = *root;
     while(curr->next != NULL){
         curr = curr->next;
     }
+    pthread_mutex_unlock(&lock);
     return curr->value;
 }
 
 char* POP(Node** root){
+    pthread_mutex_lock(&lock);
     if ((*root) == NULL){
         printf("ERROR: the stack is empty");
         exit(1);
@@ -44,11 +51,14 @@ char* POP(Node** root){
         curr = curr->next;
     }
     curr->next = NULL;
+    pthread_mutex_unlock(&lock);
     return curr->next->value;
 }
 void CLEAR(Node** root){
+    pthread_mutex_lock(&lock);
     (*root)->next = NULL;
     for(int i = 0; i < 1024; i++){
         (*root)->value[i] = '\0';
     }
+    pthread_mutex_unlock(&lock);
 }
